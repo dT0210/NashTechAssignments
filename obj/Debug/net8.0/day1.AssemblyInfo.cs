8d4da7a340f3e0947b341fb6dab818699d53b1b5
dT0210 <duthanh41025@gmail.com>
2024-04-27 00:01:42 +0700

    bin and obj files

--- obj/Debug/net8.0/day1.AssemblyInfo.cs
+++ obj/Debug/net8.0/day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97c99e9d6b2d9b66cd991b3e2e5deeb60306b9f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb3492f39fe448cf9fdb2ac80d0922b7be11b68a")]
 [assembly: System.Reflection.AssemblyProductAttribute("day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
