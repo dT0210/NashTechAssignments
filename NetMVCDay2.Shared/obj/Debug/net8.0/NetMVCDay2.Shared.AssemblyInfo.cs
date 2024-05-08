5b680e5baffd0dd04c006500bb07260f05685d38
dT0210 <duthanh41025@gmail.com>
2024-05-08 10:10:32 +0700

    Correct title and add cofirm for edit person page

--- NetMVCDay2.Shared/obj/Debug/net8.0/NetMVCDay2.Shared.AssemblyInfo.cs
+++ NetMVCDay2.Shared/obj/Debug/net8.0/NetMVCDay2.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetMVCDay2.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00d0bba8dc9f895ebaedcb569cde5bb2a7672136")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetMVCDay2.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetMVCDay2.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
